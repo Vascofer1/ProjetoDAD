a8a04884a2260fd1ba58d91d9a5966d5dbf40efe
Pedro Trindade <lightnextpt@gmail.com>
2024-11-19 13:20:54 +0000

    Login

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -1,12 +1,14 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/users/me', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::post('/auth/logout', [AuthController::class, 'logout']);
+    Route::post('/auth/refreshtoken', [AuthController::class, 'refreshToken']);
+    Route::get('/users/me', [UserController::class , 'showMe']);
+   });
 
 Route::post('/auth/login', [AuthController::class, 'login']);
