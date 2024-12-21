acb3b720c502be2d6cf2d2b72d1a081668dcf8b9
2221858 <2221858@my.ipleiria.pt>
2024-12-21 04:32:54 +0000

    n sei

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -18,7 +18,7 @@ Route::middleware(['auth:sanctum'])->group(function () {
     Route::post('/auth/logout', [AuthController::class, 'logout']);
     Route::post('/auth/refreshtoken', [AuthController::class, 'refreshToken']);
     Route::get('/users/me', [UserController::class , 'showMe']);
-    Route::put('/users/{user}', [UserController::class, 'update']);//->can('update', 'user'); 
+    Route::put('/users/{user}', [UserController::class, 'update'])->can('update', 'user'); 
     //Route::delete('/users/{user}', [UserController::class, 'destroy'])->can('delete', 'user');
     Route::get('/users', [UserController::class, 'index'])->can('view', User::class);
     Route::get('/users/{user}', [UserController::class, 'show']);
